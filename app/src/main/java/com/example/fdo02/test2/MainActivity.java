b392e2f3663a9f0dc1ed25e9ef149baf67c50652
frankvandoorn <f.vandoorn@saxion.nl>
2017-05-11 10:43:40 +0200

    First commit

---
+++ app/src/main/java/com/example/fdo02/test2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fdo02.test2;
+
+//hallo
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
